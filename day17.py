ba15d8814c1a6d82c9a0951296ca5d5106449a12
Prasad-init <prasadkhairnar11@gmail.com>
2025-08-31 21:24:09 +0530

    Create day17.py

---
+++ day17.py
@@ -0,0 +1,24 @@
+import math
+
+def prime_factorization(n):
+    factors = []
+    
+    while n % 2 == 0:
+        factors.append(2)
+        n = n // 2
+        
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n = n // i
+            
+    if n > 2:
+        factors.append(n)
+        
+    return factors
+
+# Example usage:
+N = 18
+result = prime_factorization(N)
+print(result)
+
